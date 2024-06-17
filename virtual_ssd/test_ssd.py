20647ab8bbe1644e0de1b57fbe84b70500d14143
Hyunwoo Park <dshw.park@samsung.com>
2024-06-18 00:35:05 +0900

    ssd/write: add UT for ssd write

--- virtual_ssd/test_ssd.py
+++ virtual_ssd/test_ssd.py
@@ -1,6 +1,27 @@
 from abc import ABC
+from ssd import SSD
 
 
 class TestSSD(ABC):
     def test_print(self):
         pass
+
+    def test_write_normal(self):
+        self.ssd = SSD()
+
+        addr = 20
+        value = 0x1234ABCD
+
+        ret = self.ssd.write(addr, value)
+
+        self.assertEqual(ret, "SUCCESS")
+
+    def test_write_invalid_addr(self):
+        self.ssd = SSD()
+
+        addr = 101
+        value = 0x1234ABCD
+
+        ret = self.ssd.write(addr, value)
+
+        self.assertEqual(ret, "INVALID_ADDR")
