07db5aa14dee6d222c32aead8cc8535a60723e17
doit14.jung <doit14.jung@samsung.com>
2024-06-18 14:11:39 +0900

    [refactoring] add constant for temp variable

--- test_shell_app/test_shell.py
+++ test_shell_app/test_shell.py
@@ -6,6 +6,9 @@ from unittest.mock import patch, Mock
 from shell import Shell
 
 TEST_VAL = "0x000000FF"
+TEST_ADDR = 10
+LARGE_ADDR = 9999
+NEG_ADDR = -10
 
 
 class TestShell(TestCase):
@@ -14,21 +17,21 @@ class TestShell(TestCase):
 
     def test_read_and_write(self):
         val = TEST_VAL
-        self.shell.write(addr=10, val=val)
-        self.assertEqual(val, self.shell.read(addr=10))
+        self.shell.write(addr=TEST_ADDR, val=val)
+        self.assertEqual(val, self.shell.read(addr=TEST_ADDR))
 
     def test_read_valid_input(self):
         mk = Mock(spec=Shell)
         mk.read.return_value = TEST_VAL
 
-        self.assertEqual(TEST_VAL, mk.read(10))
+        self.assertEqual(TEST_VAL, mk.read(TEST_ADDR))
 
     @patch("sys.stdout", new_callable=io.StringIO)
     def test_read_invalid_input_pos(self, mock_stdout):
-        self.assertEqual("", self.shell.read(9999))
+        self.assertEqual("", self.shell.read(LARGE_ADDR))
         self.assertEqual(mock_stdout.getvalue(), "INVALID PARAMETER\n")
 
     @patch("sys.stdout", new_callable=io.StringIO)
     def test_read_invalid_input_neg(self, mock_stdout):
-        self.assertEqual("", self.shell.read(-10))
+        self.assertEqual("", self.shell.read(NEG_ADDR))
         self.assertEqual(mock_stdout.getvalue(), "INVALID PARAMETER\n")
