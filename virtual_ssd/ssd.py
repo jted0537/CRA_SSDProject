2f1cb22181724992a91f77ef003ff481e765b2a5
doit14.jung <doit14.jung@samsung.com>
2024-06-18 11:19:35 +0900

    update: add initial code

--- virtual_ssd/ssd.py
+++ virtual_ssd/ssd.py
@@ -1,5 +1,16 @@
 import sys
 
 
+class SSD:
+    def __init__(self):
+        pass
+
+    def read(self, addr):
+        pass
+
+    def write(self, addr, value):
+        pass
+
+
 if __name__ == "__main__":
     print(sys.argv[0])
---
+++ virtual_ssd/test_ssd.py
@@ -0,0 +1,6 @@
+from abc import ABC
+
+
+class TestSSD(ABC):
+    def test_print(self):
+        pass
