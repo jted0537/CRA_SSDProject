f26eb52546daaff7de021a6eff3ecc6a1f2aae8d
doit14.jung <doit14.jung@samsung.com>
2024-06-18 14:03:40 +0900

    [refactoring] remove main code

--- test_shell_app/main.py
+++
@@ -1,9 +0,0 @@
-import os
-
-with open("../result.txt") as file_data:
-    print(file_data.readline(), end="")
-
-os.popen(f"ssd R 3").read().encode("UTF-8")
-# p = subprocess.Popen("ssd R 3", stdout=subprocess.PIPE)
-# result = p.communicate()
-# text = result[0].decode("utf-8")
