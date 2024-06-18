432ede470338c2b8bc66ffedbd032a39833afa97
doit14.jung <doit14.jung@samsung.com>
2024-06-18 13:36:47 +0900

    feature: add readtest invalid input

--- test_shell_app/shell.py
+++ test_shell_app/shell.py
@@ -1,9 +1,29 @@
+import os
+from subprocess import PIPE, Popen
+
+
 class Shell:
     def write(self, addr, val):
         pass
 
     def read(self, addr):
-        pass
+        if addr < 0 or addr > 99:
+            print("INVALID PARAMETER", flush=True)
+            return -1
+
+        try:
+            _, stderr = Popen(
+                "ssd R 3", shell=True, stdout=PIPE, stderr=PIPE
+            ).communicate()
+            if stderr != "":
+                raise Exception("stderr")
+            ret = os.popen(f"ssd R {addr}").read()
+            with open("../result.txt") as file_data:
+                val = file_data.readline()
+                print(val, end="")
+            return val
+        except Exception:
+            print(f"EXCEPTION OCCUR")
 
     def exit(self):
         pass
--- test_shell_app/test_shell.py
+++ test_shell_app/test_shell.py
@@ -1,6 +1,29 @@
-from abc import ABC
+# from abc import ABC
+import io
+from unittest import TestCase
+from unittest.mock import patch, Mock
+
+from shell import Shell
 
 
-class TestShell(ABC):
+class TestShell(TestCase):
+    def setUp(self):
+        self.shell = Shell()
+
     def test_print(self):
         pass
+
+    # @patch("sys.stdout", new_callable=io.StringIO)
+    def test_read_valid_input(self):
+        mk = Mock(spec=Shell)
+        self.shell.read(10)
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_read_invalid_input_pos(self, mock_stdout):
+        self.assertEqual(-1, self.shell.read(9999))
+        self.assertEqual(mock_stdout.getvalue(), "INVALID PARAMETER\n")
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_read_invalid_input_neg(self, mock_stdout):
+        self.assertEqual(-1, self.shell.read(-10))
+        self.assertEqual(mock_stdout.getvalue(), "INVALID PARAMETER\n")
