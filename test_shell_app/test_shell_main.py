2e83b831aa5b20f35fa37548758c1b66f08b5b4e
seungwoo <lirtha@naver.com>
2024-06-18 14:12:25 +0900

    shell/main: argument 있는 명령어 처리 테스트 추가

--- test_shell_app/test_shell_main.py
+++ test_shell_app/test_shell_main.py
@@ -5,6 +5,10 @@ from unittest.mock import patch, MagicMock
 
 from test_shell_app.shell_main import ShellMain
 
+INVALID_COMMAND = "NO_COMMAND"
+
+EXIT_COMMAND = "exit"
+
 
 class TestShellMain(TestCase):
     def setUp(self):
@@ -23,7 +27,7 @@ class TestShellMain(TestCase):
 
     @patch.object(ShellMain, "get_user_input")
     def test_shell_main_invalid_command(self, mock):
-        mock.side_effect = ["NO_COMMAND", "exit"]
+        mock.side_effect = [INVALID_COMMAND, EXIT_COMMAND]
         output = io.StringIO()
         backup = sys.stdout
         sys.stdout = output
@@ -36,8 +40,16 @@ class TestShellMain(TestCase):
             sys.stdout = backup
 
     @patch.object(ShellMain, "get_user_input")
-    def test_shell_write_command_argument_check(self, mock):
-        self.shell_main.command_map["write"] = MagicMock()
-        mock.side_effect = ["write 3 0xAAAABBBB", "exit"]
-        self.shell_main.run()
-        self.shell_main.command_map["write"].assert_called_with("3", "0xAAAABBBB")
+    def test_shell_command_argument_check(self, mock):
+        test_command_map = {
+            "write": ("write 3 0xAAAABBBB", ("3", "0xAAAABBBB")),
+            "read": ("read 3", ("3",)),
+            "fullwrite": ("fullwrite 0xAAAABBBB", ("0xAAAABBBB",)),
+        }
+        for command, (full_command, expected_argument) in test_command_map.items():
+            with self.subTest(f"{command} test"):
+                self.shell_main.command_map[command] = MagicMock()
+                shell_command = self.shell_main.command_map[command]
+                mock.side_effect = [full_command, EXIT_COMMAND]
+                self.shell_main.run()
+                shell_command.assert_called_with(*expected_argument)
