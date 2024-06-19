45567fef6d8554ee59caa85eef228aa4cb3b4131
dayae001 <dayae001@naver.com>
2024-06-19 11:03:10 +0900

    feature: testapp1 unittest 생성

--- test_shell_app/test_script.py
+++ test_shell_app/test_script.py
@@ -9,11 +9,11 @@ TEST_VALUE = "0x12345678"
 class TestScript(TestCase):
     def setUp(self):
         super().setUp()
-        self.testApp1 = TestApp1()
+        self.testApp1 = TestApp1(TEST_VALUE)
         self.testApp2 = TestApp2()
 
     def test_testapp1(self):
-        pass
+        self.assertTrue(self.testApp1.run())
 
     # test TestApp2 class
     def test_testapp2(self):
