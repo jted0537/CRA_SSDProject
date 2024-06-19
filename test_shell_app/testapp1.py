a8d98539f8542a903531214f0236a02d203d04ea
dayae001 <dayae001@naver.com>
2024-06-19 11:36:31 +0900

    refactoring: testapp1 함수 추출

--- test_shell_app/testapp1.py
+++ test_shell_app/testapp1.py
@@ -10,23 +10,35 @@ class TestApp1(Script):
         self.val = val
 
     def run(self):
+
+        self.full_write_test()
+
+        full_read_result, full_read_dict = self.shell.full_read()
+        if full_read_result == Shell.FAIL:
+            return False
+
+        self.compare_write_read(full_read_dict)
+
+        print("TestApp1 Succeed")
+        return True
+
+    def full_write_test(self):
         full_write_result = self.shell.full_write(self.val)
         if full_write_result == Shell.FAIL:
             print("TestApp1 Full Write Failed")
             return False
 
+    def full_read_test(self):
         full_read_result, full_read_dict = self.shell.full_read()
-
         if full_read_result == Shell.FAIL:
             print("TestApp1 Full Read Failed")
-            return False
+            return full_read_result, full_read_dict
+        return full_read_result, full_read_dict
 
+    def compare_write_read(self, full_read_dict):
         for addr in range(Shell.MAX_ADDR):
             if full_read_dict[addr] != self.val:
                 print(
                     f"Address: {addr}, expected {self.val}, got {full_read_dict[addr]}"
                 )
                 return False
-
-        print("TestApp1 Succeed")
-        return True
