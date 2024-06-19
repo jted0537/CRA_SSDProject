8b5b1e4938810a4786cc0af4540f2b6fc6122f5f
doit14.jung <doit14.jung@samsung.com>
2024-06-19 10:17:17 +0900

    feature: add test_app_2 test

--- test_shell_app/test_script.py
+++ test_shell_app/test_script.py
@@ -1,11 +1,19 @@
 from unittest import TestCase
 
+from testapp1 import TestApp1
+from testapp2 import TestApp2
+
 
 class TestScript(TestCase):
-    # test TestApp1 class
+    def setUp(self):
+        super().setUp()
+        self.testApp1 = TestApp1()
+        self.testApp2 = TestApp2()
+
     def test_testapp1(self):
         pass
 
     # test TestApp2 class
     def test_testapp2(self):
-        pass
+        ret = self.testApp2.run()
+        [self.assertEqual(ret[i], "0x12345678") for i in range(5)]
--- test_shell_app/testapp2.py
+++ test_shell_app/testapp2.py
@@ -1,6 +1,18 @@
 from script import Script
+from shell import Shell
+
+FIRST_VALUE = "0xAAAABBBB"
+SECOND_VALUE = "0x12345678"
 
 
 class TestApp2(Script):
+    def __init__(self):
+        super().__init__()
+        self.shell = Shell()
+
     def run(self):
-        pass
+        [self.shell.write(j, FIRST_VALUE) for i in range(30) for j in range(5)]
+
+        [self.shell.write(i, SECOND_VALUE) for i in range(5)]
+
+        return [self.shell.read(i) for i in range(5)]
