5113af6e10d2e7fb04e7523e013c4e9509486237
doit14.jung <doit14.jung@samsung.com>
2024-06-19 10:18:34 +0900

    refactoring: add testApp2 constant

--- test_shell_app/test_script.py
+++ test_shell_app/test_script.py
@@ -3,6 +3,8 @@ from unittest import TestCase
 from testapp1 import TestApp1
 from testapp2 import TestApp2
 
+TEST_VALUE = "0x12345678"
+
 
 class TestScript(TestCase):
     def setUp(self):
@@ -16,4 +18,4 @@ class TestScript(TestCase):
     # test TestApp2 class
     def test_testapp2(self):
         ret = self.testApp2.run()
-        [self.assertEqual(ret[i], "0x12345678") for i in range(5)]
+        [self.assertEqual(ret[i], TEST_VALUE) for i in range(5)]
