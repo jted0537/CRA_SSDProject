2ad0582770f0d8c7e84381d8af64c794e14963d8
yezin.kim <yezin.kim@samsung.com>
2024-06-20 11:01:04 +0900

    feat: log file이 없는 경우 file 생성

--- test_shell_app/Utils/logger.py
+++ test_shell_app/Utils/logger.py
@@ -1,4 +1,21 @@
+import os
+from pathlib import Path
+
+
 class Logger:
+    def __init__(self, log_file="latest.log"):
+        self.log_file = log_file
+        self.make_log_file()
+
+    def make_log_file(self):
+        parent_dir = Path(__file__).resolve().parent.parent / "logs"
+        parent_dir.mkdir(parents=True, exist_ok=True)
+        log_file_path = parent_dir / self.log_file
+        if not os.path.exists(log_file_path):
+            with open(log_file_path, "w"):
+                pass
+            print(f"Log file path: {log_file_path}")
+
     def logging(self, class_name, function_name, contents):
         pass
 
@@ -10,3 +27,6 @@ class Logger:
 
     def compress(self):
         pass
+
+
+Logger()
