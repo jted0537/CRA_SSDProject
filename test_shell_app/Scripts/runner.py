f3f50135e8e5bec6d9b113768873c5801ea62ed9
dayae001 <dayae001@naver.com>
2024-06-20 10:55:15 +0900

    feature: 불러온 시나리오 리스트 실행 함수(exec_scenario_list) 생성

--- test_shell_app/Scripts/runner.py
+++ test_shell_app/Scripts/runner.py
@@ -1,3 +1,8 @@
+import sys
+import io
+import importlib
+
+
 class Runner:
     def __init__(self, file_name="run_list.lst"):
         self.scenario_list = self.get_scenario_list(file_name)
@@ -9,3 +14,20 @@ class Runner:
                 scenario_list.append(scenario.strip("\n"))
 
         return scenario_list
+
+    def exec_scenario_list(self):
+        for scenario in self.scenario_list:
+            print(f"{scenario}  ---  Run...", end="")
+            captured_output = io.StringIO()
+            sys.stdout = captured_output
+
+            val1 = eval(f"importlib.import_module('{scenario.lower()}')")
+            val2 = eval(f"val1.{scenario}()")
+            rst = val2.run()
+
+            sys.stdout = sys.__stdout__
+            if rst:
+                print("Pass")
+            else:
+                print("FAIL!")
+                break
