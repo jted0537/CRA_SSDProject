d53b65344888506e05ab5e1e0f636d8744909451
yezin.kim <yezin.kim@samsung.com>
2024-06-20 11:50:55 +0900

    feat: log rotate 기능 구현

--- test_shell_app/Utils/logger.py
+++ test_shell_app/Utils/logger.py
@@ -1,21 +1,23 @@
 import os
+from datetime import datetime
 from pathlib import Path
 
 
 class Logger:
     def __init__(self, log_file="latest.log"):
         self.log_file = log_file
+        self.log_file_path = ""
         self.make_log_file()
 
     def make_log_file(self):
         parent_dir = Path(__file__).resolve().parent.parent / "logs"
         parent_dir.mkdir(parents=True, exist_ok=True)
-        log_file_path = parent_dir / self.log_file
+        self.log_file_path = parent_dir / self.log_file
 
-        if not os.path.exists(log_file_path):
-            with open(log_file_path, "w"):
+        if not os.path.exists(self.log_file_path):
+            with open(self.log_file_path, "w"):
                 pass
-            print(f"Log file path: {log_file_path}")
+            print(f"Log file path: {self.log_file_path}")
 
     def logging(self, class_name, function_name, contents):
         pass
@@ -24,7 +26,20 @@ class Logger:
         pass
 
     def rotate(self):
-        pass
+        file_size = os.path.getsize(self.log_file_path)
+        print(f"file size : {file_size}")
+        if file_size > 10 * 1024:
+            current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
+            new_file_name = f"until_{current_datetime}.log"
+
+            os.rename(
+                self.log_file_path,
+                os.path.join(os.path.dirname(self.log_file_path), new_file_name),
+            )
+
+        self.make_log_file()
 
     def compress(self):
         pass
+
+
