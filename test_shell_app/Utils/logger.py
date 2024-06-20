e8ab14370afd034df69ccfe2f0811b458825e1a4
doit14.jung <doit14.jung@samsung.com>
2024-06-20 12:17:25 +0900

    feature: add logging logic

--- test_shell_app/Utils/logger.py
+++ test_shell_app/Utils/logger.py
@@ -1,9 +1,22 @@
-class Logger:
+import os
+from datetime import datetime
+
+
+class MyLogger:
     def logging(self, class_name, function_name, contents):
-        pass
+        self.write(class_name, function_name, contents)
 
     def write(self, class_name, function_name, contents):
-        pass
+        timestamp = datetime.now().strftime("%y.%m.%d %H:%M")
+
+        log_line = (
+            f"[{timestamp}] {f'{class_name}.{function_name}'.ljust(30)} : {contents}\n"
+        )
+        log_file = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "../logs/latest.log"
+        )
+        with open(log_file, "a") as f:
+            f.write(log_line)
 
     def rotate(self):
         pass
