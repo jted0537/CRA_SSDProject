832b307cb858cc89b8429980dcdbd83702beeec9
Hyunwoo Park <dshw.park@samsung.com>
2024-06-20 12:04:04 +0900

    command buffer: add get_value(read cache) and tests related to it

--- virtual_ssd/command_buffer/test_command_buffer.py
+++ virtual_ssd/command_buffer/test_command_buffer.py
@@ -29,3 +29,21 @@ class TestCommandBuffer(unittest.TestCase):
             self.cb.insert_cmd(*cmd)
 
         self.assertEqual(self.cb.get_buffer_contents(), cmds)
+
+    def test_get_value(self):
+        cmds = [
+            ("E", 10, 10),
+            ("W", 1, "0x1234FFFF"),
+            ("W", 2, "0x5678FFFF"),
+            ("W", 3, "0x9ABCFFFF"),
+            ("E", 5, 5),
+        ]
+
+        for cmd in cmds:
+            self.cb.insert_cmd(*cmd)
+
+        self.assertEqual(self.cb.get_value(1), "0x1234FFFF")
+        self.assertEqual(self.cb.get_value(2), "0x5678FFFF")
+        self.assertEqual(self.cb.get_value(3), "0x9ABCFFFF")
+        self.assertIsNone(self.cb.get_value(4))
+        self.assertIsNone(self.cb.get_value(99))
