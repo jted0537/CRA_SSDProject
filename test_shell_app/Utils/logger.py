9807f30ec12cf151dd1604c9a1f3f9c31b4eca2c
doit14.jung <doit14.jung@samsung.com>
2024-06-20 10:25:20 +0900

    feature: add initial logger

---
+++ test_shell_app/Utils/logger.py
@@ -0,0 +1,9 @@
+class Logger:
+    def logging(self, class_name, function_name, contents):
+        pass
+
+    def write(self, class_name, function_name, contents):
+        pass
+
+    def rotate(self):
+        pass
