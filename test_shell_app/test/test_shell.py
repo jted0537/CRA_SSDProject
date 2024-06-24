b46681606170e84f0c048dc298106033dc44231e
doit14.jung <doit14.jung@samsung.com>
2024-06-24 14:21:16 +0900

    refactor: change import path for parent path (virtual_ssd)

--- test_shell_app/test/test_shell.py
+++ test_shell_app/test/test_shell.py
@@ -4,7 +4,9 @@ import re
 import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+sys.path.append(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+)
 from unittest import TestCase
 from unittest.mock import patch
 
