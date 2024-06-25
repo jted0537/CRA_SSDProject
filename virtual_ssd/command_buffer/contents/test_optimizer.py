1b86480c54dd938986e328c8f47092b3e97ce240
yezin.kim <yezin.kim@samsung.com>
2024-06-25 12:27:43 +0900

    feat: 시연용 test code 추가

--- virtual_ssd/command_buffer/contents/test_optimizer.py
+++ virtual_ssd/command_buffer/contents/test_optimizer.py
@@ -1,102 +1,47 @@
 import unittest
+from unittest import skip
+
 from optimizer import *
 
 
 class TestOptimizer(unittest.TestCase):
-    def test_write_duplication(self):
+    def test_ignore_write_1(self):
         cmds = [
-            ("W", 1, "0x1"),
-            ("W", 1, "0x2"),
-            ("W", 1, "0x3"),
-            ("W", 2, "0x4"),
-            ("W", 2, "0x5"),
-            ("W", 3, "0x6"),
+            ("W", 20, "0xABCDABCD"),
+            ("W", 21, "0x12341234"),
+            ("W", 20, "0xEEEEFFFF"),
         ]
         expected = [
-            ("W", 1, "0x3"),
-            ("W", 2, "0x5"),
-            ("W", 3, "0x6"),
+            ("W", 21, "0x12341234"),
+            ("W", 20, "0xEEEEFFFF"),
         ]
 
         optimizer = ReduceWriteDuplication()
 
         self.assertEqual(optimizer.optimize(cmds), expected)
 
-    def test_erase_after_write(self):
+    def test_ignore_write_2(self):
         cmds = [
-            ("W", 1, "0x1"),
-            ("W", 1, "0x2"),
-            ("W", 1, "0x3"),
-            ("W", 2, "0x4"),
-            ("W", 2, "0x5"),
-            ("W", 3, "0x6"),
-            ("E", 2, 1),
+            ("W", 20, "0xABCDABCD"),
+            ("W", 21, "0x12341234"),
+            ("E", 18, 5),
         ]
         expected = [
-            ("W", 1, "0x1"),
-            ("W", 1, "0x2"),
-            ("W", 1, "0x3"),
-            ("W", 3, "0x6"),
-            ("E", 2, 1),
+            ("E", 18, 5),
         ]
 
         optimizer = ReduceWriteByErase()
 
         self.assertEqual(optimizer.optimize(cmds), expected)
 
-    def test_erase_duplication(self):
-        cmds = [
-            ("E", 1, 3),
-            ("E", 5, 2),
-            ("E", 1, 10),
-            ("E", 30, 10),
-        ]
-        expected = [
-            ("E", 1, 10),
-            ("E", 30, 10),
-        ]
-
-        optimizer = ReduceEraseDuplication()
-
-        self.assertEqual(optimizer.optimize(cmds), expected)
-
     def test_merge_erase_1(self):
         cmds = [
-            ("E", 1, 3),
-            ("E", 3, 5),
-            ("E", 20, 6),
-            ("E", 26, 5),
-        ]
-        expected = [
-            ("E", 1, 7),
-            ("E", 20, 6),
-            ("E", 26, 5),
-        ]
-
-        optimizer = MergeErase()
-
-        self.assertEqual(optimizer.optimize(cmds), expected)
-
-    def test_merge_erase_2(self):
-        cmds = [
-            ("E", 1, 3),
-            ("E", 3, 5),
-            ("E", 8, 1),
-        ]
-        expected = [
-            ("E", 1, 8),
-        ]
-
-        optimizer = MergeErase()
-
-        self.assertEqual(optimizer.optimize(cmds), expected)
-
-    def test_merge_erase_3(self):
-        cmds = [
-            ("E", 12, 3),
+            ("W", 20, "0xABCDABCD"),
             ("E", 10, 2),
+            ("E", 12, 3),
         ]
         expected = [
+            ("W", 20, "0xABCDABCD"),
             ("E", 10, 5),
         ]
 
@@ -104,22 +49,7 @@ class TestOptimizer(unittest.TestCase):
 
         self.assertEqual(optimizer.optimize(cmds), expected)
 
-    def test_merge_erase_4(self):
-        cmds = [
-            ("E", 18, 5),
-            ("E", 10, 5),
-            ("E", 18, 4),
-        ]
-        expected = [
-            ("E", 10, 5),
-            ("E", 18, 5),
-        ]
-
-        optimizer = MergeErase()
-
-        self.assertEqual(optimizer.optimize(cmds), expected)
-
-    def test_shrink_erase_1(self):
+    def test_narrow_range_of_erase_1(self):
         cmds = [
             ("E", 10, 4),
             ("E", 40, 5),
@@ -137,7 +67,7 @@ class TestOptimizer(unittest.TestCase):
 
         self.assertEqual(optimizer.optimize(cmds), expected)
 
-    def test_shrink_erase_2(self):
+    def test_narrow_range_of_erase_2(self):
         cmds = [
             ("E", 50, 1),
             ("E", 40, 5),
@@ -152,20 +82,31 @@ class TestOptimizer(unittest.TestCase):
 
         self.assertEqual(optimizer.optimize(cmds), expected)
 
-    def test_shrink_erase_3(self):
+    def test_merge_erase(self):
         cmds = [
             ("E", 18, 5),
-            ("W", 20, "0xABCDABCD"),
-            ("W", 22, "0xABCDABCD"),
-            ("W", 18, "0x12341234"),
+            ("E", 10, 5),
+            ("E", 18, 4),
         ]
         expected = [
-            ("E", 19, 3),
-            ("W", 20, "0xABCDABCD"),
-            ("W", 22, "0xABCDABCD"),
-            ("W", 18, "0x12341234"),
+            ("E", 10, 5),
+            ("E", 18, 5),
         ]
 
-        optimizer = ShrinkErase()
+        optimizer = MergeErase()
 
         self.assertEqual(optimizer.optimize(cmds), expected)
+
+    def test_erase_duplication(self):
+        cmds = [
+            ("E", 5, 2),
+            ("E", 1, 10),
+        ]
+        expected = [
+            ("E", 1, 10),
+        ]
+
+        optimizer = ReduceEraseDuplication()
+
+        self.assertEqual(optimizer.optimize(cmds), expected)
+
